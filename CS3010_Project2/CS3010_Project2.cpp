303f5641a9a8b31192d51b35a27f0202bd8aca68
benja <benja@BENJAMIN-SPECTR>
2022-10-17 23:45:42 -0700

    Add project files.

---
+++ CS3010_Project2/CS3010_Project2.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Benjamin Liu
+Class: CS3010
+Assignment: Programming Project 2 
+Due Date: 10/23/2022
+Coded in C++ with Visual Studios 2022
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
