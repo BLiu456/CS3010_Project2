4f527e315978fa7408cf5b49e6db38ffbb5790c3
Benjamin Liu <benjaminliu456@gmail.com>
2022-10-18 23:10:09 -0700

    Added more to user/file input

--- CS3010_Project2/CS3010_Project2.cpp
+++ CS3010_Project2/CS3010_Project2.cpp
@@ -15,18 +15,20 @@ Coded in C++ with Visual Studios 2022
 using namespace std;
 
 //Function Prototypes here
-void getUserInput();
-bool getFileInput();
-void checkDiagDom();
-vector<double> jacobiMethod();
-vector<double> gaussSeidel();
+void getUserInput(vector<vector<double>>&, vector<double>&, int);
+bool getFileInput(vector<vector<double>>&, vector<double>&, int);
+//void checkDiagDom();
+//vector<double> jacobiMethod();
+//vector<double> gaussSeidel();
+void printVector(vector<double>);
+void printMatrix(vector<vector<double>>, vector<double>);
 
 int main()
 {
     int numE = 0, menuOp = 0;
     double stopErr= 0.0, temp;
     vector<vector<double>> coef;
-    vector<double> bVal;
+    vector<double> bVals;
     vector<double> startSolution;
     vector<double> answers;
 
@@ -37,12 +39,13 @@ int main()
     {
         cout << "\nChoose from the following options:\n"
             "1. Enter numbers manually from command line\n"
-            "2. Load a file\n";
+            "2. Load a file\n"
+            "Enter option number: ";
         cin >> menuOp;
 
         if (menuOp == 1)
         {
-            getUserInput();
+            getUserInput(coef, bVals, numE);
             break;
         }
         else if (menuOp == 2)
@@ -75,5 +78,94 @@ int main()
     {
         cin >> temp;
         startSolution.push_back(temp);
+    }
+
+}
+
+void getUserInput(vector<vector<double>>& coef, vector<double>& bVals, int numE)
+{
+    string rowInput = "", holder = "";
+
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+
+    cout << "Enter the coefficients and b values of the equations row by row:\n";
+    for (int i = 0; i < numE; i++)
+    {
+        getline(cin, rowInput);
+        istringstream ss(rowInput);
+
+        vector<double> tempVect;
+        while (ss >> holder)
+        {
+            if (ss.eof())
+            {
+                bVals.push_back(stod(holder));
+                continue;
+            }
+
+            tempVect.push_back(stod(holder));
+        }
+        coef.push_back(tempVect);
+    }
+}
+
+bool getFileInput(vector<vector<double>>& coef, vector<double>& bVals, int numE)
+{
+    string filename = "", rowInput = "", holder = "";
+    
+    cout << "Enter file name: ";
+    cin >> filename;
+
+    ifstream matFile;
+    matFile.open(filename);
+    if (!matFile)
+    {
+        cout << "Could not open file, ending program." << endl;
+        return false;
+    }
+
+    for (int i = 0; i < numE; i++)
+    {
+        getline(matFile, rowInput);
+        istringstream ss(rowInput);
+        vector<double> tempVect;
+
+        while (ss >> holder)
+        {
+            if (ss.eof())
+            {
+                bVals.push_back(stod(holder));
+                continue;
+            }
+
+            tempVect.push_back(stod(holder));
+        }
+        coef.push_back(tempVect);
+    }
+
+    cout << "Contents of " << filename << ":" << endl;
+    printMatrix(coef, bVals);
+    matFile.close();
+    return true;
+}
+void printVector(vector<double> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v.at(i) << " ";
+    }
+    cout << endl;
+}
+
+void printMatrix(vector<vector<double>> m, vector<double> n)
+{
+    for (int i = 0; i < m.size(); i++)
+    {
+        for (int j = 0; j < m.at(i).size(); j++)
+        {
+            cout << m.at(i).at(j) << " ";
+        }
+        cout << n.at(i) << endl;
     }
 }
